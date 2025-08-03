00c242bf381b733bee613354c9fa7b5683b212a2
seonananana <skdlatjsdk@gmail.com>
2025-08-04 00:03:08 +0900

    Update app.py

--- unification-simulation/app.py
+++ unification-simulation/app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+import numpy as np
+import os  
 from PIL import Image
 from statsmodels.tsa.arima.model import ARIMA
 from logistics_calc import run_logistics_comparison
